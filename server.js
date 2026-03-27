677838d61a44813ea0c019e174744215c1b2c841
melvinnavas79-del <melvinnavas79@gmail.com>
2026-03-27 01:36:03 -0500

    Actualizar server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
-const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 const server = http.createServer(app);
@@ -14,27 +13,23 @@ let rooms = {};
 io.on("connection", (socket) => {
   console.log("Usuario conectado:", socket.id);
 
-  socket.on("create-room", () => {
-    const roomId = uuidv4();
-    rooms[roomId] = [];
-    socket.join(roomId);
-    rooms[roomId].push(socket.id);
-    socket.emit("room-created", roomId);
-  });
-
   socket.on("join-room", (roomId) => {
-    if (rooms[roomId]) {
-      socket.join(roomId);
-      rooms[roomId].push(socket.id);
-      io.to(roomId).emit("user-joined", socket.id);
-    }
-  });
+    socket.join(roomId);
 
-  socket.on("disconnect", () => {
-    console.log("Usuario desconectado:", socket.id);
+    if (!rooms[roomId]) {
+      rooms[roomId] = [];
+    }
+
+    rooms[roomId].push(socket.id);
+
+    socket.to(roomId).emit("user-connected", socket.id);
+
+    socket.on("disconnect", () => {
+      socket.to(roomId).emit("user-disconnected", socket.id);
+    });
   });
 });
 
-server.listen(3000, () => {
-  console.log("Servidor corriendo en puerto 3000");
+server.listen(4000, () => {
+  console.log("Servidor corriendo en puerto 4000");
 });
